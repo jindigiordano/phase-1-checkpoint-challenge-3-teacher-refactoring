c266130990f5a37b022c1c3140f612d0353916bd
Jin Di Giordano <jin.di.giordano@gmail.com>
2016-11-23 17:12:45 -0800

    Refactored teacher methods to be less verbose

--- apprentice_teacher.rb
+++ apprentice_teacher.rb
@@ -2,54 +2,22 @@ require_relative 'dbc_family'
 require_relative 'teacher'
 
 class ApprenticeTeacher < Teacher
-  # attr_reader :salary, :target_raise
-  # #attr_reader :age, :salary, :phase, :target_raise
-  #attr_accessor :name
 
-  # def initialize(options={})
-  #   super
-  #   # @phase = 3
-  #   # @age = options.fetch(:age, 0)
-  #   # @name = options.fetch(:name, "")
-  #   @target_raise = 800
-  # end
-
-  # def offer_high_five
-  #   "High five!"
-  # end
-
-  # def set_phase(num)
-  #   @phase = num
-  #   "Cool, I've always wanted to teach phase #{num}!"
-  # end
-
-  def teach_stuff
-    response = ""
-    response += "Listen, class, this is how everything works. "
-    response += "*drops crazy knowledge bomb* "
-    response += "... You're welcome."
-    response
+  def initialize(options={})
+    super
+    @target_raise = 800
   end
 
-  # def salary=(new_salary)
-  #   puts "This better be good!"
-  #   @salary = new_salary
-  # end
-
-  # def receive_raise(raise)
-  #   @salary += raise
-  # end
+    def teach_stuff
+    @everything_works = ". "
+    @knowledge_bomb = "crazy"
+    @youre_welcome = ""
+    super
+  end
 
   def set_performance_rating(rating)
-    response = ""
-    if rating > 80
-      response = "Yay, I'm a great employee!"
-      receive_raise(@target_raise)
-    else
-      response += "Oh, well -- thanks to this actionable, specific, and kind "
-      response += "feedback, I'll do better next time."
-    end
-    response
+    @great_rating = 80
+    super
   end
 
   def attend_training_session
