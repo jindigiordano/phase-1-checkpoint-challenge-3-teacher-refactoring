d0f7573b12bb6f046abefa8463d1ee756155051a
Jin Di Giordano <jin.di.giordano@gmail.com>
2016-11-23 17:27:53 -0800

    Further refactor Student methods

--- student.rb
+++ student.rb
@@ -3,21 +3,17 @@ require_relative 'dbc_family'
 class Student < DBCFamily
 
   def set_phase(num)
-    response = ""
     if num == @phase
-      response += "I'm doing phase #{@phase} again because "
+      response = "I'm doing phase #{@phase} again because "
       response += "I put my learning first. I'm gonna rock it!"
     else
+      @phase = num
       response = "Oooh, phase #{num}. I hope I'm ready!"
     end
-    @phase = num
-    response
   end
 
   def learn_stuff
-    response = ""
-    response += "WHOA! I've never thought of it quite like that before. "
+    response = "WHOA! I've never thought of it quite like that before. "
     response += "Now I feel like a genius!"
-    response
   end
 end
